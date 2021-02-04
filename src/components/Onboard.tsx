7112f2cea2293f88cbce1d9bc629a3a2fd9a5ece
Shaun Jorstad <jorstadsd17@gcc.edu>
2021-02-04 17:58:14 -0500

    resolves major css issues

--- src/components/Onboard.tsx
+++ src/components/Onboard.tsx
@@ -2,30 +2,28 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 
-import "../assets/styles.scss";
 import CustomCarousel from "./carousel";
+import "../assets/styles.scss";
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <span className="compName">Company Name</span>
-      <ul>
-        <li className="focus">Sign in</li>
-        <li>
-          <a href="">Contact Us</a>
-        </li>
-        <li>
-          <a href="https://github.com/Yet-Another-Data-Aggregator/yada-web">
-            Open Source Code
-          </a>
-        </li>
-      </ul>
+    <div className="custom">
+      <div className="navbar">
+        <span className="compName">Company Name</span>
+        <ul>
+          <li className="focus">Sign in</li>
+          <li>
+            <a href="">Contact Us</a>
+          </li>
+          <li>
+            <a href="https://github.com/Yet-Another-Data-Aggregator/yada-web">
+              Open Source Code
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
-}
-
-function CardCarousel() {
-  return (<div><CustomCarousel/></div>);
 }
 
 function SignInForm() {
@@ -33,34 +31,36 @@ function SignInForm() {
     event.preventDefault();
   };
   return (
-    <div className="signIn w-full h-full">
-      <h1>Sign In</h1>
-      <Form onSubmit={handleLogin}>
-        <Input
-          required
-          type="email"
-          name="email"
-          id="email"
-          placeholder="email"
-        />
-        <Input
-          required
-          type="password"
-          name="password"
-          id="password"
-          placeholder="password"
-        />
-        <Button>Sign In</Button>
-      </Form>
-    </div>
+      <div className="signIn w-full h-full">
+        <h1>Sign In</h1>
+        <Form onSubmit={handleLogin}>
+          <Input
+            required
+            type="email"
+            name="email"
+            id="email"
+            placeholder="email"
+          />
+          <Input
+            required
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+          />
+          <Button>Sign In</Button>
+        </Form>
+      </div>
   );
 }
 
 function Body() {
   return (
-    <div className="grid grid-cols-2 content">
-      <CardCarousel />
-      <SignInForm />
+    <div className="custom">
+      <div className="grid grid-cols-2 content">
+        <CustomCarousel />
+        <SignInForm />
+      </div>
     </div>
   );
 }
