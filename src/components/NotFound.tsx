d61c49d4feb2ae5e13b3f1675f3390cf543ecf0a
RBOrtmann <tech.head.b@gmail.com>
2021-02-20 22:21:37 -0500

    YADA-97 trying to commit again

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -6,12 +6,27 @@ import { Link } from "react-router-dom";
 export default function NotFound() {
   return (
     <div className="notFound h-screen">
-      <div className="floatingCard cardLarge">
-        <h1>404</h1>
-        <h2>Page Not Found</h2>
-        <Link to="/">
-          <Button className="filledPrimaryButton"> Return to Sign In </Button>
-        </Link>
+      <div className="card grid grid-cols-2 md:grid-cols-2">
+        <div className="flex items-center">
+          <div>
+            <h1>404</h1>
+            <h2>Page Not Found</h2>
+          </div>
+        </div>
+        <div className="grid justify-items-end gap-4">
+          <li>
+            <Link to="/">Sign In</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/contact-us">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/request-account">Request Account</Link>
+          </li>
+        </div>
       </div>
     </div>
   );
