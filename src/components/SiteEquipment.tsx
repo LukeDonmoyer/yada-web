b946f65869d2f02814831ad72e52f7e7dd97905b
LukeDonmoyer <lukedonmoyer@gmail.com>
2021-02-23 13:39:44 -0500

    Added site and equipment interfaces, SiteEquipment page

--- src/components/SiteEquipment.tsx
+++ src/components/SiteEquipment.tsx
@@ -1,32 +1,25 @@
-import React, { ReactElement, ReactNode } from "react";
+import React, { ReactElement } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../store/rootReducer";
-import { createNewSite } from "../FireConfig";
 import DynamicNavbar, { DynamicNavLink } from "./DynamicNavbar";
-import { EquipmentUnit, SiteObject, Sites } from "../store/FirestoreInterfaces";
-import { useRouteMatch } from "react-router-dom";
-import { create } from "domain";
+import { useParams, useRouteMatch } from "react-router-dom";
+import { EquipmentUnit, SiteCollection } from "../store/FirestoreInterfaces";
 
 function addEquipment(event: any) {}
 
-interface SiteEquipmentProps {
-  siteId: keyof Sites;
-}
-
-export default function SiteEquipment({
-  siteId,
-}: SiteEquipmentProps): ReactElement {
-  const { path, url } = useRouteMatch();
-  const sites: Sites = useSelector(
+export default function SiteEquipment(): ReactElement {
+  const { siteId }: any = useParams();
+  const { url } = useRouteMatch();
+  const sites: SiteCollection = useSelector(
     (state: RootState) => state.updateSites.sites
   );
 
-  function createEquipmentLink(site: EquipmentUnit): ReactElement {
+  function createEquipmentLink(unit: EquipmentUnit): ReactElement {
     return (
       <DynamicNavLink
-        route={`/app/sites/equipment/${site.name.replace(" ", "-")}`}
-        key={site.name}
-        name={site.name}
+        route={`${url}/${unit.name.replace(" ", "-")}`}
+        key={unit.name}
+        name={unit.name}
       >
         <SiteEquipmentContent />
       </DynamicNavLink>
@@ -35,15 +28,15 @@ export default function SiteEquipment({
 
   return (
     <DynamicNavbar title={"Equipment"} buttonAction={addEquipment}>
-      {sites[siteId].equipmentUnits.map(createEquipmentLink)}
       <DynamicNavLink
-        route={`/app/sites/equipment`}
+        route={url}
         key={"default"}
         name={"default route"}
         blockLinkRender={true}
       >
         <p>Please select a piece of equipment</p>
       </DynamicNavLink>
+      {sites[siteId].equipmentUnits.map(createEquipmentLink) as any}
     </DynamicNavbar>
   );
 }
--- src/components/Sites.tsx
+++ src/components/Sites.tsx
@@ -9,11 +9,18 @@
 import { createNewSite, initializeSitesListener } from "FireConfig";
 import React from "react";
 import { useSelector } from "react-redux";
-import { Redirect, Route, useLocation } from "react-router-dom";
+import {
+  Link,
+  Redirect,
+  Route,
+  useLocation,
+  useParams,
+} from "react-router-dom";
 import { SiteObject } from "store/FirestoreInterfaces";
 import { RootState } from "store/rootReducer";
 import Content from "./Content";
 import DynamicNavbar, { DynamicNavLink } from "./DynamicNavbar";
+import SiteEquipment, { SiteEquipmentContent } from "./SiteEquipment";
 
 export default function Sites() {
   const sites = useSelector((state: RootState) => state.updateSites.sites);
@@ -25,24 +32,32 @@ export default function Sites() {
     const data = siteData as SiteObject;
     navLinks.push(
       <DynamicNavLink route={`/app/sites/${id}`} key={id} name={data.name}>
-        <p>{JSON.stringify(data)}</p>
+        <>
+          <p>{JSON.stringify(data)}</p>
+          <Link to={`/app/sites/${id}/equipment`}>Equipment</Link>
+        </>
       </DynamicNavLink>
     );
   }
 
   return (
     <div className="sites">
-      <DynamicNavbar title={"Sites"} buttonAction={createNewSite}>
-        {navLinks}
-        <DynamicNavLink
-          route={"/app/sites/"}
-          key={"default"}
-          name={"default route"}
-          blockLinkRender={true}
-        >
-          <p>Please select a site</p>
-        </DynamicNavLink>
-      </DynamicNavbar>
+      <Route path={"/app/sites/:siteId/equipment"}>
+        <SiteEquipment />
+      </Route>
+      <Route path={"/app/sites/"}>
+        <DynamicNavbar title={"Sites"} buttonAction={createNewSite}>
+          {navLinks}
+          <DynamicNavLink
+            route={"/app/sites/"}
+            key={"default"}
+            name={"default route"}
+            blockLinkRender={true}
+          >
+            <p>Please select a site</p>
+          </DynamicNavLink>
+        </DynamicNavbar>
+      </Route>
     </div>
   );
 }
