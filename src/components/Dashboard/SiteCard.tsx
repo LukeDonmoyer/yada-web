1575c40716c1e40aa63614d2588c0b208d7af55d
LukeDonmoyer <lukedonmoyer@gmail.com>
2021-03-03 22:37:03 -0500

    Added fault table, centered site cards and search bar

--- src/components/Dashboard/SiteCard.tsx
+++ src/components/Dashboard/SiteCard.tsx
@@ -21,25 +21,58 @@ export default function SiteCard({
   return (
     <div className={"card"}>
       <div>
-        <h3>{site.name}</h3>
-      </div>
-      <div>
-        <Statistic value={site.equipmentUnits.length} label={"Total units"} />
-        <Statistic
-          value={site.equipmentUnits
-            .map((unit) => unit.loggers.length)
-            .reduce(sum)}
-          label={"Total loggers"}
-        />
-      </div>
-      <div>
-        <Statistic
-          valueClassName={"fault-statistic"}
-          value={site.equipmentUnits
-            .map((site) => site.faults.length)
-            .reduce(sum)}
-          label={"Total faults"}
-        />
+        <div className={"site-information"}>
+          <div>
+            <h3>{site.name}</h3>
+          </div>
+          <div>
+            <Statistic
+              value={site.equipmentUnits.length}
+              label={"Total units"}
+            />
+            <Statistic
+              value={site.equipmentUnits
+                .map((unit) => unit.loggers.length)
+                .reduce(sum)}
+              label={"Total loggers"}
+            />
+          </div>
+          <div>
+            <Statistic
+              valueClassName={"fault-statistic"}
+              value={site.equipmentUnits
+                .map((site) => site.faults.length)
+                .reduce(sum)}
+              label={"Total faults"}
+            />
+          </div>
+        </div>
+        <div className={"fault-table"}>
+          <div className={"fault-table-column"}>
+            <h4>Equip. name</h4>
+            <div>Unit 1</div>
+            <div>Unit 2</div>
+            <div>Unit 3</div>
+          </div>
+          <div className={"fault-table-column"}>
+            <h4>Fault</h4>
+            <div>Unit 1</div>
+            <div>Unit 2</div>
+            <div>Unit 3</div>
+          </div>
+          <div className={"fault-table-column"}>
+            <h4>Equip. name</h4>
+            <div>Unit 1</div>
+            <div>Unit 2</div>
+            <div>Unit 3</div>
+          </div>
+          <div className={"fault-table-column"}>
+            <h4>#</h4>
+            <div>0</div>
+            <div>1</div>
+            <div>2</div>
+          </div>
+        </div>
       </div>
       <div className={"details-button"}>
         <Link to={`/app/sites/${siteId}`}>
