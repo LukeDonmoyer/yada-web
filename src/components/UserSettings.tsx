30990672688ff3bf2332177657ec8ef5daf3c44d
RBOrtmann <tech.head.b@gmail.com>
2021-03-06 19:38:44 -0500

    some commits

--- src/components/UserSettings.tsx
+++ src/components/UserSettings.tsx
@@ -2,25 +2,14 @@
  * Author: Brendan Ortmann
  */
 
-import React, { useState } from "react";
-import { Button, Col, Form, FormGroup, Input, Label, Row } from "reactstrap";
-import Content from "./Content";
+import { useState } from "react";
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import "../assets/styles.scss";
 import "../assets/bootstrap.scss";
 import { useSelector } from "react-redux";
 import { RootState } from "store/rootReducer";
 import { Link, Redirect } from "react-router-dom";
-import { User } from "store/FirestoreInterfaces";
-import { current } from "@reduxjs/toolkit";
 import { deleteUser, updateUserDoc } from "scripts/UserSettingsFunctions";
-
-/**
- * Use uncontrolled React form components.
- * Default form values set based on Redux state.
- * OnChange function parses form changes, calls Firestore functions.
- * Firestore changes will be reflected in Redux state,
- * but changes to state should NOT cause more rendering.
- */
 
 export default function UserSettings() {
   const uid = useSelector((state: RootState) => state.auth.userUID);
@@ -76,7 +65,7 @@ export default function UserSettings() {
   ) : (
     <div className="userSettings bootStrapStyles">
       <h1>User Settings: </h1>
-      <Form onSubmit={submitChanges} className="py-8">
+      <Form onSubmit={submitChanges}>
         <FormGroup>
           <Label for="email">Email</Label>
           <Input
