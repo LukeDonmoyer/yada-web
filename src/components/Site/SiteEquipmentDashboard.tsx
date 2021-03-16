4c51dc8e45202b8c42fc8ad735642b8016984db4
RBOrtmann <tech.head.b@gmail.com>
2021-03-15 21:29:44 -0400

    YADA-73 YADA-74 YADA-75 made functions to obtain all possible channels from loggers on unit

--- src/components/Site/SiteEquipmentDashboard.tsx
+++ src/components/Site/SiteEquipmentDashboard.tsx
@@ -5,6 +5,7 @@
  * Get channels from each template -> map channels to loggers that have them -> for each channel -> for each logger -> transform data for channel -> add data to Nivo struct -> render graph
  */
 
+import { Logger } from "@material-ui/data-grid";
 import { ReactElement } from "react";
 import { useSelector } from "react-redux";
 import dataTransformer from "scripts/DataTransformer";
@@ -16,42 +17,38 @@ export interface EquipmentDashboardProps {
   unit?: EquipmentUnit
 }
 
-export default function EquipmentDashboard({
-  loggers,
-  unit
-}: EquipmentDashboardProps): ReactElement {
-
-  let channelTemplates = useSelector((state: RootState) => state.templates);
+function getLoggersOnUnit(loggers: LoggerCollection, unit?: EquipmentUnit): LoggerObject[]{
   let loggersOnUnit: LoggerObject[] = [];
-  let channelsFromLoggers: string[] = [];
 
   for (const [id, logger] of Object.entries(loggers)){
     if (unit?.loggers.find((loggerId) => loggerId === id))
       loggersOnUnit.push(logger);
   }
 
-  for (const logger of loggersOnUnit){
+  return loggersOnUnit;
+}
+
+function getChannelsFromLoggers(loggers: LoggerObject[], channelTemplates: ChannelTemplateCollection): string[] {
+  let channelsFromLoggers: string[] = [];
+  
+  for (const logger of loggers){
     for (const key in channelTemplates[logger.channelTemplate].keys){
       if (!channelsFromLoggers.includes(key))
         channelsFromLoggers.push(key);
     }
   }
 
-  // const loggerNames = () => {
-  //   let loggerNames = [];
+  return channelsFromLoggers;
+}
 
-  //   for (const logger of loggersOnUnit){
-  //     for (const key in channelTemplates[logger.channelTemplate].keys){
-  //       loggerNames.push(
-  //         <div>
-  //           <li>{ key }</li>
-  //         </div>
-  //       );
-  //     }
-  //   }
+export default function EquipmentDashboard({
+  loggers,
+  unit
+}: EquipmentDashboardProps): ReactElement {
 
-  //   return loggerNames;
-  // }
+  let channelTemplates = useSelector((state: RootState) => state.templates);
+  let loggersOnUnit: LoggerObject[] = getLoggersOnUnit(loggers, unit);
+  let channelsFromLoggers: string[] = getChannelsFromLoggers(loggersOnUnit, channelTemplates);
 
   return (
     <div>
