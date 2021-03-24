944194aa17302f18a49adff324c505af30285942
LukeDonmoyer <lukedonmoyer@gmail.com>
2021-03-23 23:00:08 -0400

    Switched to custom table

--- src/components/ChannelTemplate/ChannelList.tsx
+++ src/components/ChannelTemplate/ChannelList.tsx
@@ -1,5 +1,12 @@
 import { ReactElement, useState } from 'react';
 import { ChannelTemplate } from '../../store/FirestoreInterfaces';
+import {
+    Dropdown,
+    DropdownItem,
+    DropdownMenu,
+    DropdownToggle,
+} from 'reactstrap';
+import { setLoggerChannelTemplate } from '../../scripts/Implementation';
 
 interface ChannelListProps {
     // The template object to get data from.
@@ -11,63 +18,155 @@ export default function ChannelList({
 }: ChannelListProps): ReactElement {
     function createRows(channels: Map<string, string>) {
         const rows: ReactElement[] = [];
+        let counter = 1;
 
         for (let [key, value] of Object.entries(channels)) {
-            rows.push(<ChannelRow name={key} scriptFile={value} />);
+            rows.push(
+                <ChannelRow
+                    name={key}
+                    scriptFile={value}
+                    keys={template.keys}
+                    index={counter++}
+                />
+            );
         }
 
         return rows;
     }
 
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Channel Name</th>
-                    <th>Script</th>
-                </tr>
-            </thead>
-            <tbody>{createRows(template.channels)}</tbody>
-        </table>
+        <div className={'scriptTable'}>
+            <div className={'row headerHighlight'}>
+                <div className={'column'}>
+                    <div className={'headerAlign'}>
+                        <h2>Channel Name</h2>
+                    </div>
+                </div>
+                <div className={'column'}>
+                    <h2>Script</h2>
+                </div>
+            </div>
+            <div className={'scrollable'}>{createRows(template.channels)}</div>
+        </div>
     );
 }
 
 interface ChannelRowProps {
     name: string;
     scriptFile: string;
+    index: number;
+    keys: Map<string, string>;
 }
 
-function ChannelRow({ name, scriptFile }: ChannelRowProps): ReactElement {
+function ChannelRow({
+    name,
+    scriptFile,
+    keys,
+    index,
+}: ChannelRowProps): ReactElement {
     const [expanded, setExpanded] = useState(false);
+
+    function createKeys(keys: Map<string, string>) {
+        const result: { name: string; type: string }[] = [];
+        for (let [key, value] of Object.entries(keys)) {
+            result.push({ name: key, type: value });
+        }
+
+        return result;
+    }
 
     return (
         <>
-            <tr>
-                <td>
-                    {expanded ? (
-                        <svg
-                            onClick={() => setExpanded(!expanded)}
-                            className={'expandIcon'}
-                            viewBox="0 0 24 24"
-                        >
-                            <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                        </svg>
-                    ) : (
-                        <svg
-                            onClick={() => setExpanded(!expanded)}
-                            className={'expandIcon'}
-                            viewBox="0 0 24 24"
-                        >
-                            <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                        </svg>
-                    )}
-                    {name}
-                </td>
-                <td>{scriptFile}</td>
-            </tr>
-            <div className={`${expanded ? 'expandContent' : 'hidden'}`}>
-                Hello World
+            <div className={`row ${index % 2 === 0 ? 'highlight' : ''}`}>
+                <div className={'column'}>
+                    <div className={''}>
+                        {expanded ? (
+                            <svg
+                                onClick={() => setExpanded(!expanded)}
+                                viewBox="0 0 24 24"
+                            >
+                                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                            </svg>
+                        ) : (
+                            <svg
+                                onClick={() => setExpanded(!expanded)}
+                                viewBox="0 0 24 24"
+                            >
+                                <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                            </svg>
+                        )}
+                        {name}
+                    </div>
+                </div>
+                <div className={'column'}>{scriptFile}</div>
+            </div>
+            <div
+                className={`${expanded ? 'expandable' : 'hidden'} ${
+                    index % 2 === 0 ? 'highlight' : ''
+                }`}
+            >
+                <div className={'row'}>
+                    <div className={'column'}>
+                        <h2>Value Name</h2>
+                    </div>
+                    <div className={'column'}>
+                        <h2>:</h2>
+                    </div>
+                    <div className={'column'}>
+                        <h2>Value Type</h2>
+                    </div>
+                </div>
+                {createKeys(keys).map(({ name, type }) => (
+                    <KeyType name={name} type={type} />
+                ))}
             </div>
         </>
+    );
+}
+
+interface KeyTypeProps {
+    name: string;
+    type: string;
+}
+
+function KeyType({ name, type }: KeyTypeProps): ReactElement {
+    const [dropDownOpen, setDropDownOpen] = useState(false);
+    const [selectedType, setSelectedType] = useState(type);
+
+    function createDropDownItem(name: string): ReactElement {
+        return (
+            <DropdownItem
+                onClick={() => {
+                    setSelectedType('boolean');
+                    //TODO: replace with setting channel type
+                }}
+            >
+                {name}
+            </DropdownItem>
+        );
+    }
+
+    return (
+        <div className={'row'}>
+            <div className={'column'}>
+                <div>{name}</div>
+            </div>
+            <div className={'column'}>:</div>
+            <div className={'column'}>
+                <div className={'bootStrapStyles'}>
+                    <Dropdown
+                        isOpen={dropDownOpen}
+                        toggle={() => setDropDownOpen(!dropDownOpen)}
+                    >
+                        <DropdownToggle caret>{selectedType}</DropdownToggle>
+                        <DropdownMenu>
+                            {['boolean', 'string', 'number', 'date'].map(
+                                createDropDownItem
+                            )}
+                        </DropdownMenu>
+                    </Dropdown>
+                </div>
+            </div>
+        </div>
     );
 }
