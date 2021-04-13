bbe4973db858eb6cb5faeaaa6ee3aabe14c3be3a
RBOrtmann <tech.head.b@gmail.com>
2021-04-13 11:28:10 -0400

    YADA-159 refactored site addition to work like template addition

--- src/components/Site/Sites.tsx
+++ src/components/Site/Sites.tsx
@@ -7,7 +7,7 @@
  */
 
 import { createNewSite } from 'scripts/Datastore';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import { SiteObject } from 'store/FirestoreInterfaces';
@@ -15,6 +15,8 @@ import { RootState } from 'store/rootReducer';
 import DynamicNavbar, { DynamicNavLink } from 'components/Control/DynamicNavbar';
 import SiteEquipment from 'components/Site/SiteEquipment';
 import { SiteContent } from 'components/Site/SiteContent';
+import Modal from 'components/Control/Modal';
+import Button, { ButtonType } from 'components/Control/Button';
 
 /**
  * Container for Sites components.
@@ -23,6 +25,32 @@ import { SiteContent } from 'components/Site/SiteContent';
  */
 export default function Sites() {
     const sites = useSelector((state: RootState) => state.sites);
+    const [newSiteName, setNewSiteName] = useState<string>('');
+    const [showSiteModal, setShowSiteModal] = useState(false);
+
+    // Shows the modal for creating new template
+    function showModal(): void {
+        setShowSiteModal(true);
+    }
+
+    // Hides the modal fro creating new template.
+    function hideModal(): void {
+        setNewSiteName('');
+        setShowSiteModal(false);
+    }
+
+    // Handles the modal done button.
+    function handleModalDone(): void {
+        if (newSiteName) {
+            newSite();
+        }
+        hideModal();
+    }
+
+    // This function creates a new template
+    function newSite() {
+        createNewSite(newSiteName);
+    }
 
     // Create links for dynamic navbar
     let navLinks: ReactElement[] = [];
@@ -41,7 +69,7 @@ export default function Sites() {
                 <SiteEquipment />
             </Route>
             <Route path={'/app/sites'}>
-                <DynamicNavbar title={'Sites'} buttonAction={createNewSite}>
+                <DynamicNavbar title={'Sites'} buttonAction={showModal}>
                     {navLinks as any}
                     <DynamicNavLink
                         route={''}
@@ -52,6 +80,37 @@ export default function Sites() {
                         <div className={'message'}>Please select a site</div>
                     </DynamicNavLink>
                 </DynamicNavbar>
+
+                <Modal show={showSiteModal}>
+                    <div className={'content'}>
+                        <div className={'inputSection'}>
+                            <h2>Site Name:</h2>
+                            <input
+                                type={'text'}
+                                name={'siteName'}
+                                id={'siteName'}
+                                onChange={(event) => {
+                                    setNewSiteName(
+                                        event.currentTarget.value
+                                    );
+                                }}
+                                autoFocus
+                            />
+                        </div>
+                    </div>
+                    <div className={'modalButtons horizontal'}>
+                        <Button
+                            type={ButtonType.tableControl}
+                            text={'Done'}
+                            onClick={handleModalDone}
+                        />
+                        <Button
+                            type={ButtonType.warning}
+                            text={'Cancel'}
+                            onClick={hideModal}
+                        />
+                    </div>
+                </Modal>
             </Route>
         </Switch>
     );
